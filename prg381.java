2576e0c89044bb63a47a17b8b431300593d05b42
Johan Strauss <576346@student.belgiumcampus.ac.za>
2021-02-24 10:55:11 +0200

    Added 3rd message

--- prg381.java
+++ prg381.java
@@ -2,6 +2,7 @@ public class prg381{
     public static void main(String[] args) {
         System.out.println("My first git");
         System.out.println("My second git");
+        System.out.println("My third git");
     }
 
 }
