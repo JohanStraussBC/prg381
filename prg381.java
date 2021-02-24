8a79ec2ba3a3d9c98805436e3dd4ae879f6bb4e4
Johan Strauss <576346@student.belgiumcampus.ac.za>
2021-02-24 11:18:22 +0200

    Added 5th print line

--- prg381.java
+++ prg381.java
@@ -3,7 +3,8 @@ public class prg381{
         System.out.println("My first git");
         System.out.println("My second git");
         System.out.println("My third git");
-        System.out.println("My second git");
+        System.out.println("My fourth git");
+        System.out.println("My fifth git");
     }
 
 }
